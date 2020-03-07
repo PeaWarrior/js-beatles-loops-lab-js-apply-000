e598ffa3eb699ae33941ba2588bbd9f266321a63
Jackson Chen <jacksonchen171@gmail.com>
2020-03-07 13:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  assigned = []
+  for (let i = 0; i< musicians.length; i++){
+    assigned.push(musician[i]+" plays "+instruments[i])
+  }
+  return assigned
+}
